68cace6f6886eb604cb2eaaa46cd9c1822ad674c
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-21 11:29:48 +0530

    Update streamlit_app.py
    Changed the index of the data

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ streamlit.text("This is a sample text")
 import pandas as pd
 
 fruit_data= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruit_data= fruit_data.set_index('Fruit')
 
 
 streamlit.dataframe(fruit_data)
