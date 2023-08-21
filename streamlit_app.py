a822e660b0ebe4a88292dfb92732d848b882d0be
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-21 11:42:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 
 streamlit.header("This is my first streamlit application")
 streamlit.text("This is a sample text")
@@ -9,7 +9,10 @@ import pandas as pd
 fruit_data= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruit_data= fruit_data.set_index('Fruit')
 
-selected_fruits=streamlit.multiselect( options=list(fruit_data.index),placeholder="Please select fruits of your choice: ")
+
+
+
+selected_fruits=st.multiselect(label="", options=list(fruit_data.index), default=None, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, max_selections=None, placeholder="Please select fruits of your choice: ", disabled=False, label_visibility="visible")
 
 fruits_to_show= fruit_data.loc[selected_fruits]
 
