c1b0d7231d85aee3e976572b4140edebab2ef05c
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-21 11:37:31 +0530

    Update streamlit_app.py
    TypeError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
    Traceback:
    File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 552, in _run_script
        exec(code, module.__dict__)
    File "/mount/src/streamlit_app_snowflake/streamlit_app.py", line 12, in <module>
        selected_fruits=streamlit.multiselect( list(fruit_data.index),placeholder="Please select fruits of your choice: ")
    File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/metrics_util.py", line 356, in wrapped_func
        result = non_optional_func(*args, **kwargs)

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import pandas as pd
 fruit_data= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruit_data= fruit_data.set_index('Fruit')
 
-selected_fruits=streamlit.multiselect( list(fruit_data.index),placeholder="Please select fruits of your choice: ")
+selected_fruits=streamlit.multiselect( options=list(fruit_data.index),placeholder="Please select fruits of your choice: ")
 
 fruits_to_show= fruit_data.loc[selected_fruits]
 
