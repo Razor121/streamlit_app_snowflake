6cb64f7f0a4f18bc4f1293b28f943f4a0d21e40d
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-21 11:55:42 +0530

    Update streamlit_app.py
    added default

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import pandas as pd
 fruit_data= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruit_data= fruit_data.set_index('Fruit')
 
-selected_fruits=streamlit.multiselect("Please select fruits: ",list(fruit_data.index))
+selected_fruits=streamlit.multiselect("Please select fruits: ",list(fruit_data.index),["Avocado","Strawberries])
 fruits_to_show=fruit_data.loc[selected_fruits]
 streamlit.dataframe(fruits_to_show)
 
