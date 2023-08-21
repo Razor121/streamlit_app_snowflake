b3369317821df1aeb14daaea8ec1310c972e99ef
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-21 11:28:58 +0530

    Update streamlit_app.py
    added a data frame from the file which is stored in amazon s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,12 @@ streamlit.header("This is my first streamlit application")
 streamlit.text("This is a sample text")
 
 
+import pandas as pd
+
+fruit_data= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+
+streamlit.dataframe(fruit_data)
+
+
 
