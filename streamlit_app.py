fe51328282ae66fc37525329ab7f76d0f7143e24
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-23 11:54:07 +0530

    Update streamlit_app.py
    changed the index of fruityvice dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ streamlit.text(fruityvice_response.json())
 
 
 fruit_normalize= pd.json_normalize(fruityvice_response.json())
+fruit_normalize= fruit_normalize.set_index('Name')
 streamlit.dataframe(fruit_normalize)
 
 
