7ea30929d1532d50a825e2317805ddbc2bbda567
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-23 20:14:44 +0530

    Update jupyter.py

--- jupyter.py
+++ jupyter.py
@@ -47,7 +47,20 @@ except URLError as e:
 
 streamlit.header("The fuirt load list contains:")
 #snowflake related functions
-streamlit.button('Get fruit load list')
+
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+# add a button to load the fruit
+if streamlit.button('Get fruit load list'):
+    my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows=get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    streamlit.stop()
+
+# streamlit.button('Get fruit load list')
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
