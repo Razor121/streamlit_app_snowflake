e661f33eef9d3a33d759d4600b251851e44c3d87
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-23 11:57:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ streamlit.text(fruityvice_response.json())
 
 
 fruit_normalize= pd.json_normalize(fruityvice_response.json())
-fruit_normalize= fruit_normalize.set_index('name')
+fruit_normalize= fruit_normalize.set_index('id')
 streamlit.dataframe(fruit_normalize)
 
 
