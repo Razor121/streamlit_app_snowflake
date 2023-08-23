91ceb143bf1ce3388ea17d3c1171e0a07deba834
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-23 11:52:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 
+fruit_normalize= pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruit_normalize)
+
+
