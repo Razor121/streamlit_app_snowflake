b9518533be3c4c15109c2ecc7b85617c2ee527e4
Sajal Dadhich <105120847+Razor121@users.noreply.github.com>
2023-08-23 15:45:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruit_normalize= pd.json_normalize(fruityvice_response.json())
 fruit_normalize= fruit_normalize.set_index('id')
 streamlit.dataframe(fruit_normalize)
 
-pip install snowflake
+
 import snowflake.connector  
 
 
